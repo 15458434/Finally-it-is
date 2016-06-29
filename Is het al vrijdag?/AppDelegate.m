4c1fc1128b541884ba4f97a52b2395d443692766
Mark Cornelisse <info@markcornelisse.nl>
2016-06-29 13:31:08 +0200

    - Reopening Window now works.

--- Is het al vrijdag?/AppDelegate.m
+++ Is het al vrijdag?/AppDelegate.m
@@ -14,6 +14,44 @@
 
 @implementation AppDelegate
 
+- (IBAction)bringAllToFrontPressed:(id)sender
+{
+    NSApplication *sharedApplication = [NSApplication sharedApplication];
+    if (sharedApplication.windows.count == 0) {
+        NSStoryboard *storyboard = [NSStoryboard storyboardWithName:@"Main" bundle:nil];
+        NSWindowController *windowController = [storyboard instantiateControllerWithIdentifier:@"WindowController"];
+        [windowController showWindow:self];
+    } else {
+        if (sharedApplication.mainWindow) {
+            [sharedApplication.mainWindow makeKeyAndOrderFront:self];
+        } else if (sharedApplication.windows.firstObject) {
+            [sharedApplication.windows.firstObject makeKeyAndOrderFront:self];
+        }
+    }
+}
+
+
+#pragma mark - NSApplicationDelegate
+
+- (BOOL)applicationShouldHandleReopen:(NSApplication *)sender hasVisibleWindows:(BOOL)flag
+{
+    if (sender.windows.count == 0) {
+        NSStoryboard *storyboard = [NSStoryboard storyboardWithName:@"Main" bundle:nil];
+        NSWindowController *windowController = [storyboard instantiateControllerWithIdentifier:@"WindowController"];
+        [windowController showWindow:self];
+        return YES;
+    } else {
+        if (sender.mainWindow) {
+            [sender.mainWindow makeKeyAndOrderFront:self];
+            return YES;
+        } else if (sender.windows.firstObject) {
+            [sender.windows.firstObject makeKeyAndOrderFront:self];
+            return YES;
+        }
+    }
+    return NO;
+}
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
 }
@@ -21,5 +59,7 @@
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
 }
+
+
 
 @end
